4d2ff115380d2ab3c817cd67ffbfbb7fd7930d7d
Min <liweimin1996@qq.com>
2019-03-22 23:06:55 +0800

    Update linear_regreesion.py

--- 机器学习 吴恩达 Python代码实现/ex1-linear regression 作业1 线性回归/linear_regreesion.py
+++ 机器学习 吴恩达 Python代码实现/ex1-linear regression 作业1 线性回归/linear_regreesion.py
@@ -14,34 +14,31 @@ import numpy as np
 df = pd.read_csv('ex1data1.txt', names=['population', 'profit'])#读取数据并赋予列名
 print(df.head())#查看前5行
 print(df.info())#查看相关信息
+print(df.describe())#查看数据基本特征
 sns.lmplot('population', 'profit', df, size=6, fit_reg=False)
 plt.show()#画出相关数据
 
-def get_X(df):#读取特征
-#     """
-#     use concat to add intersect feature to avoid side effect
-#     not efficient for big dataset though
-#     """
+def get_X(df):#读取x特征
     ones = pd.DataFrame({'ones': np.ones(len(df))})#ones是m行1列的dataframe
     data = pd.concat([ones, df], axis=1)  # 合并数据，根据列合并
     return data.iloc[:, :-1].as_matrix()  # 这个操作返回 ndarray,不是矩阵
 
 
-def get_y(df):#读取标签
-#     '''assume the last column is the target'''
+def get_y(df):#读取y标签
+#默认最后一列是目标值
     return np.array(df.iloc[:, -1])#df.iloc[:, -1]是指df的最后一列
 
 
 def normalize_feature(df):
-#     """Applies function along input axis(default 0) of DataFrame."""
+#沿DataFrame的输入轴(默认值为0)应用函数
     return df.apply(lambda column: (column - column.mean()) / column.std())#特征缩放
 
 def linear_regression(X_data, y_data, alpha, epoch, optimizer=tf.train.GradientDescentOptimizer):# 这个函数是旧金山的一个大神Lucas Shen写的
-      # placeholder for graph input
+      # 图输入占位符
     X = tf.placeholder(tf.float32, shape=X_data.shape)
     y = tf.placeholder(tf.float32, shape=y_data.shape)
 
-    # construct the graph
+    # 构建图
     with tf.variable_scope('linear-regression'):
         W = tf.get_variable("weights",
                             (X_data.shape[1], 1),
@@ -50,26 +47,25 @@ def linear_regression(X_data, y_data, alpha, epoch, optimizer=tf.train.GradientD
         y_pred = tf.matmul(X, W)  # m*n @ n*1 -> m*1
 
         loss = 1 / (2 * len(X_data)) * tf.matmul((y_pred - y), (y_pred - y), transpose_a=True)  # (m*1).T @ m*1 = 1*1
+        #损失函数定义
+    opt = optimizer(learning_rate=alpha)#学习率
+    opt_operation = opt.minimize(loss)#最小化损失函数
 
-    opt = optimizer(learning_rate=alpha)
-    opt_operation = opt.minimize(loss)
-
-    # run the session
+    # 运行session
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         loss_data = []
 
         for i in range(epoch):
             _, loss_val, W_val = sess.run([opt_operation, loss, W], feed_dict={X: X_data, y: y_data})
-            loss_data.append(loss_val[0, 0])  # because every loss_val is 1*1 ndarray
+            loss_data.append(loss_val[0, 0])  # 因为每一个损失值都是1*1 ndarray
 
-            if len(loss_data) > 1 and np.abs(loss_data[-1] - loss_data[-2]) < 10 ** -9:  # early break when it's converged
-                # print('Converged at epoch {}'.format(i))
+            if len(loss_data) > 1 and np.abs(loss_data[-1] - loss_data[-2]) < 10 ** -9:  # 当满足收敛条件时跳出
                 break
 
-    # clear the graph
+    # 清理图
     tf.reset_default_graph()
-    return {'loss': loss_data, 'parameters': W_val}  # just want to return in row vector format
+    return {'loss': loss_data, 'parameters': W_val}  # 只想以行向量格式返回
 
 
 data = pd.read_csv('ex1data1.txt', names=['population', 'profit'])#读取数据，并赋予列名
@@ -92,9 +88,9 @@ def lr_cost(theta, X, y):
 
     inner = X @ theta - y  # R(m*1)，X @ theta等价于X.dot(theta)
 
-    # 1*m @ m*1 = 1*1 in matrix multiplication
-    # but you know numpy didn't do transpose in 1d array, so here is just a
-    # vector inner product to itselves
+    # 1*m @ m*1 = 1*1 矩阵乘法
+    # numpy没有在1d数组中进行转置，所以这里只有一个
+    # 向量内积
     square_sum = inner.T @ inner
     cost = square_sum / (2 * m)
 
