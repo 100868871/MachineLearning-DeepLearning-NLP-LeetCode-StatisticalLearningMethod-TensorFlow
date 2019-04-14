31e26276347f313efdb1b1d1106422976d7f996e
Min <liweimin1996@qq.com>
2019-04-14 16:36:15 +0800

    Create vec_test.py

---
+++ 深度学习 吴恩达 Python代码实现/vector test/vec_test.py
@@ -0,0 +1,65 @@
+"""
+Created on Sat Mar  9 22:02:25 2019
+
+@author: liweimin
+"""
+
+import turtle
+import random
+import time
+import jieba
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_boston
+from sklearn.datasets import load_iris
+
+
+
+a=np.random.rand(1000000)
+b=np.random.rand(1000000)
+
+start_time=time.perf_counter()
+
+c=np.dot(a,b)
+
+end_time=time.perf_counter()
+
+time=end_time-start_time
+
+print(time)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+
+
+
+
+
+
+    
+
+
+
+       
+    
+    
+
